9a0d7544ccdc236d3314a91a10448c72ada4c64e
tommy <trosenblatt@itba.edu.ar>
2020-09-04 20:48:38 -0300

    minisat y grep funcionan en slave

--- slave.c
+++ slave.c
@@ -1,53 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
 #include <unistd.h>
 
 #define READ 0
 #define WRITE 1
 
+void miniGrep(char *filename);
+
 int main(int argc, char *argv[]) {
+  int cant;
   char *line = NULL;
   size_t linecap = 0;
   ssize_t linelen;
 
-  pid_t minisat_slave;
-  FILE *stream_ms, *stream_grep;
-  char output[4096], ret_output[4096];
+  for (cant = 1; cant < argc; cant++)
+    miniGrep(argv[cant]);
 
-  //while ((linelen = getline(&line, &linecap, stdin)) > 0) {
-  int mypipe_read[2], mypipe_write[2];
-  pipe(mypipe_read);
-  pipe(mypipe_write);
-  minisat_slave = fork();
-  if (minisat_slave == -1) {
-    perror("fork\n");
-    exit(EXIT_FAILURE);
-  } else if (minisat_slave == 0) {
-    close(mypipe_read[WRITE]);
-    close(mypipe_write[READ]);
-    execlp("/bin/grep", "grep", "-o", "-e", "\"Number of.*[0-9]\\+\"", "-e", "\"CPU time.*\"", "-e", ".*SATISFIABLE", (char *)NULL);
-    exit(EXIT_FAILURE);
-  } else {
-    close(mypipe_read[READ]);
-    close(mypipe_write[WRITE]);
-    stream_ms = popen("minisat CNF/hole6.cnf", "r");
-    if (stream_ms == NULL) {
-      printf("popen minisat\n");
-      exit(EXIT_FAILURE);
-    }
-    while (fgets(output, 4096, (FILE *)stream_ms) != NULL) {
-      printf("%s", output);
-      if (write(mypipe_read[WRITE], output, 4096) == -1) {
-        printf("Fallo\n");
-        exit(1);
-      }
-    }
-    printf("hola\n");
-    pclose(stream_ms);
-    close(mypipe_read[WRITE]);
-    close(mypipe_write[READ]);
+  while ((linelen = getline(&line, &linecap, stdin)) > 0) {
+    miniGrep(line);
   }
-  // }
+
   exit(EXIT_SUCCESS);
+}
+
+void miniGrep(char *filename) {
+  FILE *stream;
+  char output[1024] = {}, msg[1024] = {}, cmd[1024] = {};
+  
+  sprintf(cmd, "minisat %s | grep -o -e \"Number of .*[0-9]\\+\" -e \"CPU time.*\" -e \".*SATISFIABLE\"", filename);
+  stream = popen(cmd, "r");
+  if (stream == NULL) {
+    printf("popen minisat\n");
+    exit(EXIT_FAILURE);
+  }
+  while (fgets(output, 1024, (FILE *)stream) != NULL) {
+    strcat(msg, output);
+  }
+  printf("%s", msg);
+  pclose(stream);
 }
